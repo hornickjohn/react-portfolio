a550935de155413df3887f6a89331723cde68469
John Hornick <jhornick@live.com>
2023-03-04 04:09:50 -0500

    Footer, aboutme pic added.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 //TODO remove props if unused
 function Footer(props) {
-    
+    return (
+        <footer className="center-content">
+            <a href="https://github.com/hornickjohn" target="_blank"><button>GitHub</button></a>
+            <a href="https://www.linkedin.com/in/john-hornick-2724063a/" target="_blank"><button>LinkedIn</button></a>
+            <a href="https://stackoverflow.com/users/21330083/jhornick" target="_blank"><button>Stack Overflow</button></a>
+        </footer>
+    );
 }
 
 export default Footer;
