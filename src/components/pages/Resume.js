39c21dcafbdec7ceddaab3d64cc0b2d79e7d6a91
John Hornick <jhornick@live.com>
2023-03-06 04:56:20 -0500

    resume page content

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,28 @@ import React from 'react';
 
 export default function Resume() {
     return (
-        <p>Resume page.</p>
+        <div>
+            <h2>Resume</h2>
+            <a href="/port_file/sampleresume.docx" download><button>Download Full Version</button></a>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>Responsive Design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>mySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+            </ul>
+        </div>
     );
 }
