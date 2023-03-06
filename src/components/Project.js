ee6d6a47f78a65a6975f895694bdef35ac8ca8e7
John Hornick <jhornick@live.com>
2023-03-06 07:38:13 -0500

    ghpages deploy install and image debugging

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,7 @@ function Project(props) {
         let margin = "60%";
         if(!props.project.deployedLink) { margin="40%"; }
         return (<a href={link} style={{marginRight:margin, marginBottom:'8%'}} target="_blank">
-        <img src="/port_img/ghlogo.png" alt="Github Repo" title="Github Repo" />
+        <img src="./port_img/ghlogo.png" alt="Github Repo" title="Github Repo" />
         </a>);
     }
     function getDeployedLinkTag(link) {
@@ -14,14 +14,14 @@ function Project(props) {
         let margin = "20%";
         if(!props.project.repoLink) { margin="40%"; }
         return (<a href={link} style={{marginRight:margin, marginBottom:'8%'}} target="_blank">
-        <img src="/port_img/deployedlogo.png" alt="Deployed Site" title="Deployed Site" />
+        <img src="./port_img/deployedlogo.png" alt="Deployed Site" title="Deployed Site" />
         </a>);
     }
 
     return(
     <div className="card">
         <figure className="hovereffect">
-            <img src={"/port_img/" + props.project.img} alt={props.project.imgAlt} />
+            <img src={"./port_img/" + props.project.img} alt={props.project.imgAlt} />
             <div className="fc">{props.project.name}<br /><span>{props.project.techs}</span></div>
             {getRepoLinkTag(props.project.repoLink)}
             {getDeployedLinkTag(props.project.deployedLink)}
